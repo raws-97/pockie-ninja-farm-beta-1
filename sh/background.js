5a65bd58cb47a9903aea1ab81bbcd091e1a8b05f
rawsplay <rahmatmedia3@gmail.com>
2024-07-22 11:10:43 +0700

    remove sh folder

--- sh/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(function(details) {
-    const link = "https://www.paypal.com/donate/?hosted_button_id=WBGKBJ73EDAW2";  // Substitua com o link desejado
-
-    if (details.reason === "install" || details.reason === "update") {
-        chrome.tabs.create({ url: link });
-    }
-});
