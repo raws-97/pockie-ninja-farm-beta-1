9c5e1a34f080a0214dbd807f13c50293bf4a9ca8
rawsplay <rahmatmedia3@gmail.com>
2024-08-05 23:35:02 +0700

    fix run bat

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ CALL pip install -r requirements.txt
 CALL SET PLAYWRIGHT_BROWSERS_PATH=0
 CALL playwright install chromium
 CALL playwright install firefox
-CALL pyinstaller -y -F mainMenuP.py --add-data "venv/lib/site-packages/playwright/driver;playwright/driver" --add-data "venv/Lib/site-packages/sv_ttk;sv_ttk/" --add-data "venv/Lib/site-packages/sv_ttk/sv.tcl;."
+CALL pyinstaller -y -F main.py --add-data "venv/lib/site-packages/playwright/driver;playwright/driver" --add-data "venv/Lib/site-packages/sv_ttk;sv_ttk/" --add-data "venv/Lib/site-packages/sv_ttk/sv.tcl;."
